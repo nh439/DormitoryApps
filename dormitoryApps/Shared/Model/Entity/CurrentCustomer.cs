a69bdd2237d6538be12cfe9071848a7a3db67a7c
nh439 <nithi.handsome@gmail.com>
2022-01-25 21:33:40 +0700

    Add RoomCustomer

---
+++ dormitoryApps/Shared/Model/Entity/CurrentCustomer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dormitoryApps.Shared.Model.Entity
+{
+    public class CurrentCustomer
+    {
+		public string Id { get; set; }
+		public int RoomId { get; set; }
+		public string Firstname { get; set; }
+		public string Surname { get; set; }
+		public DateTime Stayed { get; set; }
+		public byte IsMain { get; set; }
+		public string Address { get; set; }
+		public decimal Rental { get; set; }
+		public DateTime ContractDate { get; set; }
+	}
+}
---
+++ dormitoryApps/Shared/Model/Entity/PastCustomer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dormitoryApps.Shared.Model.Entity
+{
+    public class PastCustomer
+    {
+		public string Id { get; set; }
+		public int RoomId { get; set; }
+		public string Firstname { get; set; }
+		public string Surname { get; set; }
+		public DateTime Stayed { get; set; }
+		public byte IsMain { get; set; }
+		public string Address { get; set; }
+		public decimal Rental { get; set; }
+		public DateTime ContractDate { get; set; }
+		public DateTime StayUntil { get; set; }
+		public decimal Deposit { get; set; }
+		public byte Refunded { get; set; }
+	}
+}
