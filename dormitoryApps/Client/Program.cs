4006de2aa9cfd93dd1c3135c041fd834310007cd
nh439 <nithi.handsome@gmail.com>
2022-01-26 18:31:46 +0700

    Add Incomplete

--- dormitoryApps/Client/Program.cs
+++ dormitoryApps/Client/Program.cs
@@ -1,14 +1,22 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using dormitoryApps.Client;
 using dormitoryApps.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.Modal;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+builder.Services.AddSweetAlert2();
+builder.Services.AddBlazoredModal();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DepartmentServices>();
 builder.Services.AddScoped<OfficerServices>();
+
+
 
 await builder.Build().RunAsync();
