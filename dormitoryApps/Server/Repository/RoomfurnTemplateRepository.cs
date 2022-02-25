4cfd3a52aad2367c3972ac387c887fd3544bbb2e
nh439 <nithi.handsome@gmail.com>
2022-02-25 18:08:01 +0700

    Add RoomTemplate Services

---
+++ dormitoryApps/Server/Repository/RoomfurnTemplateRepository.cs
@@ -0,0 +1,62 @@
+﻿using dormitoryApps.Server.Databases;
+using dormitoryApps.Shared.Model.Entity;
+using RocketSQL;
+
+namespace dormitoryApps.Server.Repository
+{
+    public class RoomfurnTemplateRepository
+    {
+        private readonly DBConnection _databases;
+        private const string TableName = "roomfurntemplate";
+        public RoomfurnTemplateRepository(DBConnection databases)
+        {
+            _databases = databases;
+        }
+        public async Task<bool> Create(RoomfurnTemplate template)
+        {
+            var res = await _databases.Dorm.InsertEntitiesAsync<RoomfurnTemplate>(template);
+            return res;
+        }
+        public async Task<int> Create(IEnumerable<RoomfurnTemplate> templates)
+        {
+            var res = await _databases.Dorm.InsertEntitiesAsync<RoomfurnTemplate>(templates);
+            return res;
+        }
+        public async Task<bool> Update(RoomfurnTemplate template)
+        {
+            var res = await _databases.Dorm.UpdateEntityAsync<RoomfurnTemplate>(template,$"Id={template.Id}");
+            return res == 1;
+        }
+        public async Task<bool> Delete(long Id)
+        {
+            var res = await _databases.Dorm.DeleteAsync(TableName,$"Id={Id}");
+            return res == 1;
+        }
+        public async Task<int> DeleteTemplate(int roomId)
+        {
+            var res = await _databases.Dorm.DeleteAsync(TableName,$"RoomId={roomId}");
+            return res;
+        }
+        public async Task<List<RoomfurnTemplate>> Getall()
+        {
+            var res = await _databases.Dorm.SelectEntitiesAsync<RoomfurnTemplate>();
+            return res.ToList();
+        }
+        public async Task<List<RoomfurnTemplate>> GetByRoomId(int roomId)
+        {
+            var res = await _databases.Dorm.SelectEntitiesAsync<RoomfurnTemplate>(TableName,$"RoomId={roomId}");
+            return res.ToList();
+        }
+        public async Task<List<RoomfurnTemplate>> GetByType(string typeName)
+        {
+            var res = await _databases.Dorm.SelectEntitiesAsync<RoomfurnTemplate>(TableName,$"Type='{typeName}'");
+            return res.ToList();
+        }
+        public async Task<RoomfurnTemplate> GetById(int id)
+        {
+            var res = await _databases.Dorm.SelectEntitiesAsync<RoomfurnTemplate>(TableName, $"Id={id}");
+            return res;
+        }
+
+    }
+}
---
+++ dormitoryApps/Shared/Model/Entity/RoomTemplate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dormitoryApps.Shared.Model.Entity
+{
+    public class RoomTemplate
+    {
+		public int Id { get; set; }
+		public string RoomName { get; set; }
+		public decimal? Size { get; set; }
+		public byte Aircond { get; set; }
+		public byte WaterHeater { get; set; }
+		public byte TV { get; set; }
+		public byte Fan { get; set; }
+		public decimal Rental { get; set; }
+		public byte Furniture { get; set; }
+		public byte Fridge { get; set; }
+		public byte Enabled { get; set; }
+		public virtual List<RoomfurnTemplate> Furnitures { get; set; }
+	}
+}
