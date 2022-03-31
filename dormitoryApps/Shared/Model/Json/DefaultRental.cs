25d758638dd68b6eab0a175891c72e4735adb290
nh439 <nithi.handsome@gmail.com>
2022-03-31 15:50:30 +0700

    Finish Water

---
+++ dormitoryApps/Shared/Model/Json/DefaultRental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dormitoryApps.Shared.Model.Json
+{
+    public class DefaultRental
+    {
+        public decimal BookingFee { get; set; }
+        public decimal Taxpercentage { get; set; }
+        public decimal ContractFee { get; set; }
+        public bool Readonly { get; set; }
+    }
+}
---
+++ dormitoryApps/Shared/Model/Json/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dormitoryApps.Shared.Model.Json
+{
+    public class Utilities
+    {
+        public decimal WaterPrice { get; set; }
+        public decimal ElectricityPrice { get; set; }
+        public bool Readonly { get; set; }
+    }
+}
