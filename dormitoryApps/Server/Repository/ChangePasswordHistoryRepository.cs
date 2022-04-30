857f6800034ded8070a7d3e81f17cb1e113150dc
nithi.rocket@outlook.com <nithi.rocket@outlook.com>
2022-04-30 18:16:20 +0700

    Add Changepassword

---
+++ dormitoryApps/Server/Repository/ChangePasswordHistoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using dormitoryApps.Server.Databases;
+
+namespace dormitoryApps.Server.Repository
+{
+    public class ChangePasswordHistoryRepository
+    {
+        private readonly DBConnection _databases;
+        private const string TableName = "changepasswordhistory";
+        public ChangePasswordHistoryRepository(DBConnection databases)
+        {
+            _databases = databases;
+        }
+
+    }
+}
