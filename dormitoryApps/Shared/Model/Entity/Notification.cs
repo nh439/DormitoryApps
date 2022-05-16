40de7aac607e575871686c7e4cd50b3ad33c1330
nithi.rocket@outlook.com <nithi.rocket@outlook.com>
2022-05-16 20:51:58 +0700

    Add Notification Objects

---
+++ dormitoryApps/Shared/Model/Entity/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dormitoryApps.Shared.Model.Entity
+{
+    public class Notification
+    {
+		public string Id { get; set; }
+		public string Header { get; set; }
+		public string Detail { get; set; }
+		public DateTime Date { get; set; }
+		public Int64? SenderId { get; set; }
+		public string SenderFrom { get; set; }
+		public bool SendAll { get; set; }
+		public virtual List<NotificationAttendee> Attendees { get; set; }
+		public virtual List<NotificationAttachment>? Attachments { get; set; }
+	}
+}
---
+++ dormitoryApps/Shared/Model/Entity/NotificationAttachment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dormitoryApps.Shared.Model.Entity
+{
+    public class NotificationAttachment
+    {
+		public string Id { get; set; }
+		public string NotificationId { get; set; }
+		public string FileName { get; set; }
+		public string FileExtension { get; set; }
+		public Int64 FileSize { get; set; }
+		public string FileType { get; set; }
+		public string FileContent { get; set; }
+		public DateTime FileCreateDate { get; set; }
+	}
+}
