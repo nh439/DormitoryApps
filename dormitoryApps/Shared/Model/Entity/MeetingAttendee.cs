c148baced5bd51ffaa5cc9e2e6cdf1fe2426df5f
nithi.rocket@outlook.com <nithi.rocket@outlook.com>
2022-05-23 13:18:06 +0700

    Add Meeting Model

---
+++ dormitoryApps/Shared/Model/Entity/MeetingAttendee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dormitoryApps.Shared.Model.Entity
+{
+    public class MeetingAttendee
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public Int64 MeetingId { get; set; }
+        public Int64 OfficerId { get; set; }
+    }
+}
